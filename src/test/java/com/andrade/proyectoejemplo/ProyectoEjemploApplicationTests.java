2c270e3889a51a50d56c2bd5a0ece81d879b9e14
Joweeell <oswelandrade@outlook.es>
2026-09-08 09:47:16 -0300

    a

---
+++ src/test/java/com/andrade/proyectoejemplo/ProyectoEjemploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrade.proyectoejemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoEjemploApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
