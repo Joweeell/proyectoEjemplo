1c570c35017187ec400ac85c2e1c2954cea4acea
Joweeell <oswelandrade@outlook.es>
2026-09-08 09:59:05 -0300

    a

---
+++ src/main/java/com/andrade/proyectoejemplo/health/HealthCheckController.java
@@ -0,0 +1,4 @@
+package com.andrade.proyectoejemplo.health;
+
+public class HealthCheckController {
+}
---
+++ src/main/java/com/andrade/proyectoejemplo/health/dto/HealthResponseDto.java
@@ -0,0 +1,4 @@
+package com.andrade.proyectoejemplo.health.dto;
+
+public record HealthResponseDto() {
+}
